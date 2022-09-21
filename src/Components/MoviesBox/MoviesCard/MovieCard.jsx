4fb9db7a0581f10dd3f79819cd4533adaab18b63
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-22 01:53:19 +0300

    Added the jsx for movie card component close #8

--- src/Components/MoviesBox/MoviesCard/MovieCard.jsx
+++ src/Components/MoviesBox/MoviesCard/MovieCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
 export default function MovieCard(props) {
-  const { data } = props;
+  const { poster, title, url, rating, type } = props;
 
   return (
-    <div>MovieCard</div>
+    <section className='movie-card'>
+      <img 
+        src={poster}
+        alt='movie poster'
+        className='movie-poster'
+      />
+      <h1 className='movie-link'><a href={url}>{title}</a></h1>
+      <section className='inner-section'>
+        <h3 className='type'>{type}</h3>
+        <section className='start-section'>
+          <FaStar icon='star-icon' />
+          <p className='rating'>{rating}</p>
+        </section>
+      </section>
+    </section>
   )
 }
