34b05f4820e5be901e96d219b81a1cdd3132e6e2
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-22 01:27:23 +0300

    Added the fetch functionality for the default value and for each change on search value close #9

--- src/Components/MoviesBox/MoviesBox.jsx
+++ src/Components/MoviesBox/MoviesBox.jsx
@@ -2,22 +2,43 @@ import React, { Component } from 'react';
 
 import MovieCard  from './MoviesCard/MovieCard.jsx';
 
+import { getData } from '../../utils/getData.js';
+
 export default class MoviesBox extends Component {
   constructor (props) {
     super(props);
     this.state = {
       data: [],
+      err: '',
     };
-    this.search = props.search;
+  }
+
+  componentDidMount() {
+    getData('https://yts.mx/api/v2/list_movies.jsonp?limit=20')
+    .then((movies) => this.setState({ data: movies.data.movies }))
+    .catch((err) => this.setState((prevState, prevProps) => ({ ...prevState, err })));
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.search !== prevProps.search) {
+      getData(`https://yts.mx/api/v2/list_movies.jsonp?query_term=${this.props.search}`)
+        .then((movie) => this.setState((prevState, prevProps) => {
+          console.log(movie.data);
+          return { data: movie.data.movies }
+        }))
+        .catch((err) => this.setState((prevState, prevProps) => ({ ...prevState, err })));
+    }
   }
 
   render() {
-    return (
+    console.log(this.state.data);
+    return !this.state.data ?
+    <div className='fall-back'>No data found!!!</div> :
+    (
     <section className='movies-container'>
       {
-        // console.log();
-      this.state.data.map((movie)=> (
-        <MovieCard
+      this.state.data.map((movie)=> {
+        return <MovieCard
           key={movie.id}
           poster={movie.medium_cover_image}
           title = {movie.title}
@@ -25,7 +46,7 @@ export default class MoviesBox extends Component {
           rating={movie.rating}
           type={movie.genres[0]} 
         />
-      ))
+      })
       }
     </section>
       )
