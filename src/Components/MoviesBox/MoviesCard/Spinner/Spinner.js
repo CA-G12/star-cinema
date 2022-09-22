d3bfeabbe871a68262ff675a67c1538d93710578
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-22 15:20:43 +0300

    Added the spinner component and styles it close #18

---
+++ src/Components/MoviesBox/MoviesCard/Spinner/Spinner.js
@@ -0,0 +1,20 @@
+import './Spinner.css';
+
+const Spinner = (props) => {
+  const { isReturned } = props;
+
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
